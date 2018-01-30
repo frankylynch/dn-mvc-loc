b4b61527e921042068000c86d2ee3f7d3a25799d
Frank Lynch <franklynch@Franks-MacBook-Air.local>
2018-01-30 19:06:38 +0000

    small changes

---
+++ Models/Grouping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace dnmvcloc.Models
+{
+    public class Grouping
+    {
+        public Grouping() { }
+
+        public enum GroupingTypeEnum { Item = 1, Location, User }
+
+        public GroupingTypeEnum GroupType { get; set; }
+
+        public int GroupingId { get; set; }
+        public int GroupingItemId { get; set; }
+
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace dnmvcloc.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+         
+        }
+        public int OrderId
+        {
+            get;
+            set;
+        }
+
+        public DateTime OrderTime
+        {
+            get;
+            set;
+        }
+
+    }
+}
